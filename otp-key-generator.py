e4e6ba891323c7b6722281b7313200e8466bcfb1
CreativeCoder615 <uiuu3014@gmail.com>
2024-08-24 16:17:13 +0330

    Add files via upload

---
+++ otp-key-generator.py
@@ -0,0 +1,29 @@
+import argparse, secrets, string
+
+parser = argparse.ArgumentParser(prog='OTP-Key-Generator', description='This program can generate secure and random keys for OTP')
+parser.add_argument('-n', '--number', help='The number of keys', required=True, metavar='<number>', type=int)
+parser.add_argument('-x', '--hex', action='store_true', help='Create hexadecimal keys')
+parser.add_argument('-o', '--output', help='The name of output file', metavar='<file>', required=True)
+parser.add_argument('-l', '--length', help='The length of generated keys', metavar='<length>', required=True, type=int)
+args = parser.parse_args()
+
+KEY_LETTERS = tuple(string.ascii_letters + string.punctuation + string.digits)
+
+def create_random_key():
+    key = ''
+    key_length = args.length
+    if not args.hex:
+        for i in range(key_length):
+            key += secrets.choice(KEY_LETTERS)
+    else:
+        key = secrets.token_hex(key_length)
+    return key
+
+if __name__ == '__main__':
+    print('Generating Keys ...')
+    with open(args.output, 'w') as f:
+        for i in range(args.number):
+            print(f'{i+1}/{args.number}', end='\r')
+            f.write(create_random_key() + '\n\n')
+            f.flush()
+    print('Done.' + ' ' * (len(f'{args.number}/{args.number}') - 5))
